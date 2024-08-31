b325770a0b79a967eb1ac1a7edb450552433c518
TanX-009 <tanmaymuley009@gmail.com>
2024-08-31 13:45:08 +0530

    fix(sql): fix lossy normalization

--- sql/99_final_db_dump.sql
+++ sql/99_final_db_dump.sql
@@ -1,77 +1,82 @@
-ALTER TABLE user_home
-RENAME TO user_home_old;
-
+-- create user table
 CREATE TABLE user (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE address (
-    address_id INT AUTO_INCREMENT PRIMARY KEY,
-    street_address VARCHAR(255) NOT NULL,
-    state VARCHAR(255) NOT NULL,
-    zip VARCHAR(10) NOT NULL,
-    UNIQUE(street_address, state, zip)
+-- create state table
+CREATE TABLE state (
+  state_id INT AUTO_INCREMENT PRIMARY KEY,
+  state_name VARCHAR(255) UNIQUE NOT NULL
 );
 
+-- create home table with foreign key to state
 CREATE TABLE home (
-    home_id INT AUTO_INCREMENT PRIMARY KEY,
-    address_id INT,
-    sqft DECIMAL(10, 2) NOT NULL,
-    beds INT NOT NULL,
-    baths INT NOT NULL,
-    list_price DECIMAL(15, 2) NOT NULL,
-    FOREIGN KEY (address_id) REFERENCES address(address_id)
+  home_id INT AUTO_INCREMENT PRIMARY KEY,
+  state INT,
+  street_address VARCHAR(255) NOT NULL,
+  zip VARCHAR(10) NOT NULL,
+  sqft DECIMAL(10, 2) NOT NULL,
+  beds INT NOT NULL,
+  baths INT NOT NULL,
+  list_price DECIMAL(15, 2) NOT NULL,
+  FOREIGN KEY (state) REFERENCES state(state_id)
 );
 
-CREATE TABLE user_home (
-    user_id INT,
-    home_id INT,
-    PRIMARY KEY(user_id, home_id),
-    FOREIGN KEY (user_id) REFERENCES user(user_id),
-    FOREIGN KEY (home_id) REFERENCES home(home_id)
-);
+-- add data from user_home table to state table
+INSERT INTO state (state_name)
+SELECT DISTINCT state FROM user_home;
 
-
+-- add data from user_home table to user table
 INSERT INTO user (username, email)
 SELECT DISTINCT username, email
-FROM user_home_old;
+FROM user_home;
 
-INSERT INTO address (street_address, state, zip)
-SELECT DISTINCT street_address, state, zip
-FROM user_home_old;
-
-INSERT INTO home (address_id, sqft, beds, baths, list_price)
-SELECT 
-    address.address_id,
-    user_home_old.sqft,
-    user_home_old.beds,
-    user_home_old.baths,
-    user_home_old.list_price
+-- add data from user_home table to home table with foreign key to state
+INSERT INTO home (
+  state,
+  street_address,
+  zip,
+  sqft,
+  beds,
+  baths,
+  list_price
+)
+SELECT DISTINCT
+    s.state_id,
+    h.street_address,
+    h.zip,
+    h.sqft,
+    h.beds,
+    h.baths,
+    h.list_price
 FROM 
-    user_home_old
+    user_home h
 JOIN 
-    address ON user_home_old.street_address = address.street_address
-              AND user_home_old.state = address.state
-              AND user_home_old.zip = address.zip;
+    (SELECT * FROM state ORDER BY state_name) s ON h.state = s.state_name;
 
-INSERT INTO user_home (user_id, home_id)
+-- rename user_home table to user_home_old
+ALTER TABLE user_home
+RENAME TO user_home_old;
+
+-- create new user_home table with foreign keys to user and home
+CREATE TABLE user_home (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user INT,
+    home INT,
+    FOREIGN KEY (user) REFERENCES user(user_id),
+    FOREIGN KEY (home) REFERENCES home(home_id)
+);
+
+-- add data from user_home_old table to user_home table
+INSERT INTO user_home (user, home)
 SELECT 
-    user.user_id,
-    home.home_id
+    u.user_id, 
+    h.home_id
 FROM 
-    user_home_old
+    user_home_old uho
 JOIN 
-    user ON user_home_old.username = user.username
-          AND user_home_old.email = user.email
+    user u ON u.username = uho.username AND u.email = uho.email
 JOIN 
-    home ON home.sqft = user_home_old.sqft
-               AND home.beds = user_home_old.beds
-               AND home.baths = user_home_old.baths
-               AND home.list_price = user_home_old.list_price
-JOIN 
-    address ON address.address_id = home.address_id
-              AND user_home_old.street_address = address.street_address
-              AND user_home_old.state = address.state
-              AND user_home_old.zip = address.zip;
+    home h ON h.street_address = uho.street_address 
